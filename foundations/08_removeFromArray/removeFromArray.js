2402187c9623d304477fbe432f6f03d3961e09cc
Matthew <bulaongmatthew55432@gmail.com>
2026-08-30 10:41:33 +0800

    Complete foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,33 @@
-const removeFromArray = function() {
+//Declare a new variable for the new array (non-immutability)
+//find the element that corresponds to the remover arg
+//Delete the element and shift it to the left (use splice)
+
+//for multiple arguments
+//turn the arguments into an array
+//first index is the array
+//consequent args are the elements to be remove
+//use for loop to iterate every multiple remove args
+
+//uses rest arguments
+const removeFromArray = function (array, ...remover) {
+  let newArray;
+  let arrayIndexToRemove;
+
+  //outer to check for the array length
+  for (let i = 0; i < array.length; i++) {
+    //inner to check remover length
+    for (let j = 0; j < remover.length; j++) {
+      arrayIndexToRemove = array.indexOf(remover[j]);
+      if (arrayIndexToRemove != -1) {
+        newArray = array.splice(arrayIndexToRemove, 1);
+      }
+    }
+  }
+
+  return array;
 };
+
+//[3,2,2,3]
 
 // Do not edit below this line
 module.exports = removeFromArray;
