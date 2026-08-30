5c912c03bc1d4ef4c180472f20b51dc128b2decf
Matthew <bulaongmatthew55432@gmail.com>
2026-08-30 13:51:56 +0800

    Complete foundations/11_tempConversions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFarenheit) {
+  tempCelcius = ((tempFarenheit - 32) * 5) / 9;
+
+  return +tempCelcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelcius) {
+  tempFarenheit = (tempCelcius * 9) / 5 + 32;
+
+  return +tempFarenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
