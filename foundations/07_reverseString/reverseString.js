595434c9e1ac423dbb0b9c254cdd4021aa54e3a2
Matthew <bulaongmatthew55432@gmail.com>
2026-08-30 08:52:46 +0800

    Complete foundations/ 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+//Declare variable to save reversed string
+//Get length of the string
+//Use loop to get index of the last character
+//Save the character to the reverse variable
+//Do this until all of the string have been reversed
 
+const reverseString = function (string) {
+  let reversedString = "";
+  let stringLength = string.length;
+  for (let i = stringLength; i > 0; i--) {
+    reversedString += string[i - 1];
+    console.log(reversedString);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
